57c07769f92dff024bcb3f00ed0508c096afe141
Gabriel Britain <quontas@gmail.com>
2018-11-05 20:05:05 -0600

    Updated for progress report.

---
+++ src/constants.py
@@ -0,0 +1,2 @@
+DATA_PATH = '../data/'
+LABEL_COLS = ['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']
---
+++ src/functions.py
@@ -0,0 +1,46 @@
+from constants import DATA_PATH, LABEL_COLS
+
+import pandas as pd
+from sklearn import metrics
+
+
+def load_bad_words():
+    """
+    Loads Facebook's list of obscene words.
+
+    Returns:
+    - A list of strings.
+    """
+    BAD_WORDS = []
+    with open(DATA_PATH + 'bad_words.txt', 'r') as f:
+        BAD_WORDS = f.read().split('\n')
+        return BAD_WORDS
+
+
+def build_data_path(filename, use_preprocessed=False):
+    """
+    Returns the path to the desired file. Depends on whether use_preprocessed is True or False.
+    
+    Args:
+    - filename: The name of the file to build a path to. Should be in the data/ directory.
+    - use_preprocessed: Whether or not to use preprocessed data. Defaults to False.
+    Returns:
+    - A path string.
+    """
+    training_data_path = DATA_PATH
+    if use_preprocessed:
+        training_data_path += '/preprocessed/'
+    else:
+        training_data_path += '/raw/'
+    training_data_path += filename
+    
+    return training_data_path
+
+def print_report(y_truth, y_predictions, data_type='VALIDATION'):
+    hamming_loss = metrics.hamming_loss(y_truth, y_predictions, labels=LABEL_COLS)
+    jaccard = metrics.jaccard_similarity_score(y_truth, y_predictions)
+    print(f'{data_type} RESULTS:')
+    print(f'Hamming loss (lower is better): {hamming_loss}')
+    print(f'Jaccard similarity (higher is better): {jaccard}')
+    print()
+    print(metrics.classification_report(y_truth, y_predictions, target_names=LABEL_COLS))
